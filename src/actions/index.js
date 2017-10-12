7381062a723150b6688e10d37d1e1f307407ffb9
meghna-saxena <meghna.s1906@gmail.com>
2017-10-12 20:21:04 +0530

    local storage of jwt token

--- src/actions/index.js
+++ src/actions/index.js
@@ -13,6 +13,7 @@ export function signinUser({ email, password }) {
         //- update state to indicate user is auth'd
                 dispatch({type: AUTH_USER});
         //- save jwt token for user to make auth'd requests in future
+                localStorage.setItem('token', response.data.token);
         //- redirect to route '/feature'
                 browserHistory.push('/feature');
             })
