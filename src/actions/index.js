4207bd3e92ee25f8a7d71b4a5c28ac67c72b1375
meghna-saxena <meghna.s1906@gmail.com>
2017-10-12 21:08:55 +0530

    handling authError

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,9 @@
 import axios from 'axios';
 import { browserHistory } from 'react-router';
-import { AUTH_USER } from './types';
+import {
+     AUTH_USER,
+     AUTH_ERROR
+ } from './types';
 
 const ROOT_URL = 'http://localhost:3090';
 
@@ -21,6 +24,14 @@ export function signinUser({ email, password }) {
             .catch(() => {
          //If request is bad:
         //Show an error msg to user
+                dispatch(authError('Bad Login Information'));
         });
     }
+}
+
+export function authError(error) {
+    return {
+        type: AUTH_ERROR,
+        payload: error
+    };
 }
