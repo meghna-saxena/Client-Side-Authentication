9f7394376bc871346eca9917bf334bcf1b910de3
meghna-saxena <meghna.s1906@gmail.com>
2017-10-15 08:17:53 +0530

    finished validation on client

--- src/components/auth/signup.js
+++ src/components/auth/signup.js
@@ -9,27 +9,22 @@ class Signup extends Component {
             <form>
                 <fieldset className="form-group">
                     <label>Email:</label>
-                    <input className="form-control"
-                            {...email} />
-                </fieldset>
+                    <input className="form-control" {...email} />
+                    {email.touched && email.error && <div className="error">{email.error}</div>}
+                 </fieldset>
                 <fieldset className="form-group">
                     <label>Password:</label>
-                    <input className="form-control"
-                            type="password"
-                            {...password} 
-                    />
+                    <input className="form-control" {...password} type="password" />
                     {password.touched && password.error && <div className="error">{password.error}</div>}
                 </fieldset>
                 <fieldset className="form-group">
-                    <label>Confirm Password::</label>
-                    <input className="form-control"
-                            type="password"
-                            {...passwordConfirm}
-                    />
+                    <label>Confirm Password:</label>
+                    <input className="form-control" {...passwordConfirm} type="password" />
+                    {passwordConfirm.touched && passwordConfirm.error && <div className="error">{passwordConfirm.error}</div>}
                 </fieldset>
-                <button action="submit" className="btn btn-primary">Sign Up!</button>
+                <button action="submit" className="btn btn-primary">Sign up!</button>
             </form>
-        ); 
+        );
     }
 }
 
@@ -37,9 +32,18 @@ class Signup extends Component {
 function validate(formProps) {
     //make new object & attach error msg to show to user
     const errors = {};
-    if(formProps.password !== formProps.passwordConfirm) {
-        errors.password = 'Password must match';
-    }
+    if (!formProps.email) {
+    errors.email = 'Please enter an email';
+}
+    if (!formProps.password) {
+    errors.password = 'Please enter a password';
+}
+    if (!formProps.passwordConfirm) {
+    errors.passwordConfirm = 'Please enter a password confirmation';
+}
+    if (formProps.password !== formProps.passwordConfirm) {
+    errors.password = 'Passwords must match';
+}
     return errors;
 }
 
