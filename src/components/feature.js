9253a04e6c759489b7847459d8070d05b8e6f11f
meghna-saxena <meghna.s1906@gmail.com>
2017-10-15 18:02:59 +0530

    making authenticated API requests

--- src/components/feature.js
+++ src/components/feature.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
 
 class Feature extends Component {
-    render() {
-        return (
-            <div>This is a feature</div>
-        );
-    }
+  componentWillMount() {
+    this.props.fetchMessage();
+  }
+
+  render() {
+    return (
+      <div>This is a feature</div>
+    );
+  }
 }
 
-export default Feature;
+export default connect(null, actions)(Feature);
