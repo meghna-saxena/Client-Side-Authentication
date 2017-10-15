f456c4da472a1f21f1beb9020110a61b025d47e5
meghna-saxena <meghna.s1906@gmail.com>
2017-10-15 19:50:16 +0530

    handled data from auth'd requests

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,68 +1,71 @@
 import axios from 'axios';
 import { browserHistory } from 'react-router';
 import {
-     AUTH_USER,
-     UNAUTH_USER,
-     AUTH_ERROR
- } from './types';
+  AUTH_USER,
+  UNAUTH_USER,
+  AUTH_ERROR,
+  FETCH_MESSAGE
+} from './types';
 
 const ROOT_URL = 'http://localhost:3090';
 
 export function signinUser({ email, password }) {
-    return function(dispatch) {
-        //Submit email/password to server
-        axios.post(`${ROOT_URL}/signin`, { email, password })
-            .then(response => {
-        //If request is good:
-        //- update state to indicate user is auth'd
-                dispatch({type: AUTH_USER});
-        //- save jwt token for user to make auth'd requests in future
-                localStorage.setItem('token', response.data.token);
-        //- redirect to route '/feature'
-                browserHistory.push('/feature');
-            })
-
-            .catch(() => {
-         //If request is bad:
-        //Show an error msg to user
-                dispatch(authError('Bad Login Information'));
-        });
-    }
+  return function(dispatch) {
+    // Submit email/password to the server
+    axios.post(`${ROOT_URL}/signin`, { email, password })
+      .then(response => {
+        // If request is good...
+        // - Update state to indicate user is authenticated
+        dispatch({ type: AUTH_USER });
+        // - Save the JWT token
+        localStorage.setItem('token', response.data.token);
+        // - redirect to the route '/feature'
+        browserHistory.push('/feature');
+      })
+      .catch(() => {
+        // If request is bad...
+        // - Show an error to the user
+        dispatch(authError('Bad Login Info'));
+      });
+  }
 }
 
 export function signupUser({ email, password }) {
-    return function(dispatch) {
-      axios.post(`${ROOT_URL}/signup`, { email, password })
-        .then(response => {
-          dispatch({ type: AUTH_USER });
-          localStorage.setItem('token', response.data.token);
-          browserHistory.push('/feature');
-        })
-        .catch(response => dispatch(authError(response.data.error)));
-    }
+  return function(dispatch) {
+    axios.post(`${ROOT_URL}/signup`, { email, password })
+      .then(response => {
+        dispatch({ type: AUTH_USER });
+        localStorage.setItem('token', response.data.token);
+        browserHistory.push('/feature');
+      })
+      .catch(response => dispatch(authError(response.data.error)));
+  }
 }
 
 export function authError(error) {
-    return {
-      type: AUTH_ERROR,
-      payload: error
-    };
+  return {
+    type: AUTH_ERROR,
+    payload: error
+  };
 }
 
 export function signoutUser() {
-    //removes jwt upon authorization: false
-    localStorage.removeItem('token');
+    //removes jwt when authorization:false
+  localStorage.removeItem('token');
 
-    return {type: UNAUTH_USER};
+  return { type: UNAUTH_USER };
 }
 
 export function fetchMessage() {
-    return function(dispatch) {
-      axios.get(ROOT_URL, {
-        headers: { authorization: localStorage.getItem('token') }
-      })
-        .then(response => {
-          console.log(response);
+  return function(dispatch) {
+    axios.get(ROOT_URL, {
+      headers: { authorization: localStorage.getItem('token') }
+    })
+      .then(response => {
+        dispatch({
+          type: FETCH_MESSAGE,
+          payload: response.data.message
         });
-    }
+      });
+  }
 }
