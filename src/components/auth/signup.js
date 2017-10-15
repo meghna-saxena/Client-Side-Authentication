abf83d916d692c34380dd46994ebd6431b019c0e
meghna-saxena <meghna.s1906@gmail.com>
2017-10-15 09:41:31 +0530

    added signup action creator

--- src/components/auth/signup.js
+++ src/components/auth/signup.js
@@ -3,10 +3,15 @@ import{ reduxForm } from 'redux-form';
 import * as actions from '../../actions';
 
 class Signup extends Component {
+    handleFormSubmit(formProps) {
+        // Call action creator to sign up the user!
+        this.props.signupUser(formProps);
+    }
+
     render() {
         const {handleSubmit, fields: {email, password, passwordConfirm}} = this.props;
         return (
-            <form>
+            <form onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}>
                 <fieldset className="form-group">
                     <label>Email:</label>
                     <input className="form-control" {...email} />
@@ -52,4 +57,4 @@ export default reduxForm({
     fields: ['email', 'password', 'passwordConfirm'],
     //pass the validate function to reduxForm
     validate
-}) (Signup);
+}, null, actions) (Signup);
